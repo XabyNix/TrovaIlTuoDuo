a71a6aec77bf285d5e4e4050534b908771c9ffd7
xaby <xabynix@gmail.com>
2024-01-06 18:29:52 +0100

    works

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import React from "react";
+import React, { FormEvent, FormEventHandler } from "react";
 import ServerSelectMenu from "./ServerSelectMenu";
 import { Input } from "../ui/input";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useQueryState } from "next-usequerystate";
+import { Button } from "../ui/button";
 
 const SearchBar = ({ className }: { className?: string }) => {
 	const [name, setName] = useQueryState("name");
 	const params = useSearchParams();
+	const router = useRouter();
 
-	const queryParams = {} as { [key: string]: string };
-	for (const [key, value] of params.entries()) {
-		queryParams[key] = value;
-	}
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const queryParams = new URLSearchParams(params.toString());
+		queryParams.set("name", name || "");
+		router.push(`/playerslist?${queryParams.toString()}`);
+	};
 
 	return (
-		<div className={cn("flex items-center relative rounded-md", className)}>
-			<ServerSelectMenu className="absolute left-3" />
-			<Input
-				value={name || ""}
-				onChange={(value) => setName(value.currentTarget.value)}
-				className="w-full h-max py-5 px-28 bg-slate-300 hover:bg-slate-200  text-black font-bold text-md  transition-all"
-				placeholder="RiotID"
-			/>
-			<Link
-				href={{
-					pathname: "/playerslist",
-					query: { ...queryParams, name: name }, //`${params.toString()}&name=${name}`,
-				}}
-				prefetch={false}
-				className="absolute right-2 shadow-md p-3 font-semibold rounded-md bg-myRed"
-			>
-				Aiutami!
-			</Link>
-		</div>
+		<form onSubmit={handleSubmit} className={cn(className)}>
+			<div className="flex items-center relative rounded-md">
+				<ServerSelectMenu className="absolute left-3" />
+
+				<Input
+					value={name || ""}
+					onChange={(value) => setName(value.currentTarget.value)}
+					className="w-full h-max py-5 px-28 bg-slate-300 hover:bg-slate-200  text-black font-bold text-md  transition-all"
+					placeholder="Summoner Name"
+				/>
+
+				<Button
+					type="submit"
+					className="absolute right-3 h-[75%] shadow-md p-3 font-semibold rounded-md bg-myRed"
+				>
+					Aiutami!
+				</Button>
+			</div>
+		</form>
 	);
 };
 
